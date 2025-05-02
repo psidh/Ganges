d082ee1a6387d3fbb371241dc52b0cc909a80b57
P SIDHARTH <philkhanasidharth14@gmail.com>
2025-05-02 12:06:40 +0530

    2.5 parser for return statements completed
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -42,8 +42,16 @@ type LetStatement struct {
 	Value Expression
 }
 
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
