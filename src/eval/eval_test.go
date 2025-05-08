37140f58c19c7d4be324086548e96e91f2c6fdf6
P SIDHARTH <philkhanasidharth14@gmail.com>
2025-05-08 15:19:36 +0530

    3.5.2 Prefix Expressions - and ! evaluated and tested
    evalBangOperatorExpression()
    evalMinusOperatorExpression() were added
    
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/eval/eval_test.go
+++ src/eval/eval_test.go
@@ -15,6 +15,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5" ,-5},
+		{"-89" ,-89},
 	}
 
 	for _, test := range tests {
@@ -41,6 +43,41 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
 	if result.Value != expected {
 		t.Errorf("object has incorrect value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!satya", false},
+		{"!asatya", true},
+		{"!5", false},
+		{"!!satya", true},
+		{"!!asatya", false},
+		{"!!5", true},
+	}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		testBooleanObject(t, evaluated, test.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+
+	if !ok {
+		t.Errorf("object is not Boolean got=%T, (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t",
+			result.Value, expected)
 		return false
 	}
 	return true
